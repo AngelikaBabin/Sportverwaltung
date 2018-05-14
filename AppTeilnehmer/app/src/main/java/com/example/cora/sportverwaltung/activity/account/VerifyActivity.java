319220e172862196404badc1a68ee4854d24eaf4
NicoKandut <nico.kandut@gmail.com>
2018-05-14 14:11:02 +0200

    added verify activity

--- AppTeilnehmer/app/src/main/java/com/example/cora/sportverwaltung/activity/account/VerifyActivity.java
+++ AppTeilnehmer/app/src/main/java/com/example/cora/sportverwaltung/activity/account/VerifyActivity.java
@@ -1,7 +1,10 @@
 package com.example.cora.sportverwaltung.activity.account;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 import com.example.cora.sportverwaltung.R;
 
 public class VerifyActivity extends AppCompatActivity {
@@ -10,5 +13,12 @@ public class VerifyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify);
+
+        findViewById(R.id.button_login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(VerifyActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
