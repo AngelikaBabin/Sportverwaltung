c54d5801958571959524bf961f4034b610767cf1
NicoKandut <nico.kandut@gmail.com>
2018-05-16 08:55:17 +0200

    added assets

--- AppTeilnehmer/app/src/main/java/com/example/cora/sportverwaltung/businesslogic/connection/DatabaseConnection.java
+++ AppTeilnehmer/app/src/main/java/com/example/cora/sportverwaltung/businesslogic/connection/DatabaseConnection.java
@@ -31,7 +31,7 @@ public class DatabaseConnection {
     public static DatabaseConnection getInstance() {
         if (connection == null) {
             try {
-                URL url = new URL("http", "192.168.193.150", 8080, "SportVerwaltung_WebServices/webresources");
+                URL url = new URL("http", "192.168.194.107", 8080, "SportVerwaltung_WebServices/webresources");
                 connection = new DatabaseConnection(url);
             } catch (MalformedURLException ex) {
                 ex.printStackTrace();
@@ -43,8 +43,6 @@ public class DatabaseConnection {
     private DatabaseConnection(URL url) {
         this.url = url;
     }
-
-    public DatabaseConnection() { }
 
     //region USER
     public int register(Account account) throws Exception {
