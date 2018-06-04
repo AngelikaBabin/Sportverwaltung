9a404d04b9627504c7519d6abb5068418d254d92
KristianRaj <Rajic-Kristian9560@gmx.at>
2018-06-04 14:49:23 +0200

    Fixing Nicos Bugs

--- AppTeilnehmer/app/src/main/java/com/example/cora/sportverwaltung/activity/events/InfoMyEventsActivity.java
+++ AppTeilnehmer/app/src/main/java/com/example/cora/sportverwaltung/activity/events/InfoMyEventsActivity.java
@@ -7,12 +7,18 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.cora.sportverwaltung.businesslogic.connection.AsyncTaskHandler;
+import com.example.cora.sportverwaltung.businesslogic.connection.AsyncWebserviceTask;
 import com.example.cora.sportverwaltung.R;
 import com.example.cora.sportverwaltung.activity.base.ConnectionActivity;
 import com.example.cora.sportverwaltung.businesslogic.data.Veranstaltung;
 import com.google.gson.Gson;
+import android.app.ProgressDialog;
 
-public class InfoMyEventsActivity extends ConnectionActivity {
+import static com.example.cora.sportverwaltung.businesslogic.misc.HttpMethod.DELETE;
+import static com.example.cora.sportverwaltung.businesslogic.misc.HttpMethod.POST;
+
+public class InfoMyEventsActivity extends ConnectionActivity implements AsyncTaskHandler {
     TextView textView_header, textView_date, textView_place, textView_organizer, textView_details, textView_sport;
     Button button_logout;
 
@@ -53,7 +59,8 @@ public class InfoMyEventsActivity extends ConnectionActivity {
             @Override
             public void onClick(View view) {
                 try {
-                    connection.departicipate(selectedEvent.getId());
+                    AsyncWebserviceTask task = new AsyncWebserviceTask(DELETE, "teilnahme", InfoMyEventsActivity.this);
+                    task.execute("eventId=" + selectedEvent.getId(), null);
                     Toast.makeText(InfoMyEventsActivity.this, "You are no longer participating", Toast.LENGTH_LONG).show();
                     button_logout.setEnabled(false);
                 } catch (Exception e) {
@@ -62,5 +69,29 @@ public class InfoMyEventsActivity extends ConnectionActivity {
                 }
             }
         });
+    }
+
+
+    @Override
+    public void onPreExecute() {
+        progDialog = new ProgressDialog(this);
+        progDialog.setMessage("Logging in...");
+        progDialog.setIndeterminate(false);
+        progDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progDialog.setCancelable(false);
+        progDialog.show();
+    }
+
+    @Override
+    public void onSuccess(int statusCode, String content) {
+        progDialog.dismiss();
+        button_logout.setEnabled(false);
+        Toast.makeText(this, "You are now participating", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onError(Error err) {
+        progDialog.cancel();
+        Toast.makeText(this, err.getMessage(), Toast.LENGTH_SHORT).show();
     }
 }
