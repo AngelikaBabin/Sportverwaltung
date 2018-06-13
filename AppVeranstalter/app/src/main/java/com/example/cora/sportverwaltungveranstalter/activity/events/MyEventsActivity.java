e69e38d8d201ce8f792b47fb93a0c883a2a17b26
AngelikaBabin <babin.angelika@gmail.com>
2018-06-13 09:27:20 +0200

    list all events from veranstalter

---
+++ AppVeranstalter/app/src/main/java/com/example/cora/sportverwaltungveranstalter/activity/events/MyEventsActivity.java
@@ -0,0 +1,30 @@
+package com.example.cora.sportverwaltungveranstalter.activity.events;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.cora.sportverwaltungveranstalter.R;
+import com.example.cora.sportverwaltungveranstalter.activity.base.BaseActivity;
+
+public class MyEventsActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContent(R.layout.content_my_events);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
