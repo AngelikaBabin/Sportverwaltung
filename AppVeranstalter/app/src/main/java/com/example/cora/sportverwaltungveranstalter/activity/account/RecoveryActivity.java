4614da3d52e25e4a7098247fce33cf2fe6d07b3c
AngelikaBabin <babin.angelika@gmail.com>
2018-06-13 08:56:27 +0200

    recvorey Activity

--- AppVeranstalter/app/src/main/java/com/example/cora/sportverwaltungveranstalter/activity/account/RecoveryActivity.java
+++ AppVeranstalter/app/src/main/java/com/example/cora/sportverwaltungveranstalter/activity/account/RecoveryActivity.java
@@ -2,6 +2,8 @@ package com.example.cora.sportverwaltungveranstalter.activity.account;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.cora.sportverwaltungveranstalter.R;
 import com.example.cora.sportverwaltungveranstalter.activity.base.ExposingActivity;
@@ -13,7 +15,20 @@ public class RecoveryActivity extends ExposingActivity implements AsyncTaskHandl
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recovery);
+        initEventhandler();
+    }
 
+    private void initEventhandler() {
+        findViewById(R.id.button_recover).setOnClickListener(view->{
+           try {
+               TextView editText_email = findViewById(R.id.editText_email);
+               String email = editText_email.getText().toString();
+           }
+           catch (Exception ex){
+               ex.printStackTrace();
+               Toast.makeText(this, ex.getMessage(), Toast.LENGTH_LONG).show();
+           }
+        });
     }
 
     @Override
