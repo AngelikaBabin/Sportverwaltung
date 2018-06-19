d06622bc5211825a113978df8d831f81a27a46d3
AngelikaBabin <babin.angelika@gmail.com>
2018-06-19 22:30:40 +0200

    http error handling app veranstalter

--- AppVeranstalter/app/src/main/java/com/example/cora/sportverwaltungveranstalter/activity/events/AddEventActivity.java
+++ AppVeranstalter/app/src/main/java/com/example/cora/sportverwaltungveranstalter/activity/events/AddEventActivity.java
@@ -1,5 +1,6 @@
 package com.example.cora.sportverwaltungveranstalter.activity.events;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -12,12 +13,13 @@ import android.widget.Toast;
 
 import com.example.cora.sportverwaltungveranstalter.R;
 import com.example.cora.sportverwaltungveranstalter.activity.base.ExposingActivity;
+import com.example.cora.sportverwaltungveranstalter.businesslogic.connection.AsyncTaskHandler;
 import com.example.cora.sportverwaltungveranstalter.businesslogic.data.Sportart;
 
 /**
  * @babin GUI
  */
-public class AddEventActivity extends ExposingActivity implements AdapterView.OnItemSelectedListener {
+public class AddEventActivity extends ExposingActivity implements AdapterView.OnItemSelectedListener, AsyncTaskHandler{
     EditText editText_title, editText_date, editText_location, editText_participators, editText_details;
     Spinner spinner_sports;
     Button button_save;
@@ -65,5 +67,39 @@ public class AddEventActivity extends ExposingActivity implements AdapterView.On
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
+    }
+
+    @Override
+    public void onPreExecute() {
+        progDialog = new ProgressDialog(this);
+        progDialog.setMessage("Adding new Event....");
+        progDialog.setIndeterminate(false);
+        progDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progDialog.setCancelable(false);
+        progDialog.show();
+    }
+
+    @Override
+    public void onSuccess(int statusCode, String content) {
+        progDialog.dismiss();
+        switch (statusCode) {
+            case 201:
+                button_save.setEnabled(false);
+                Toast.makeText(AddEventActivity.this, "You are added a Event", Toast.LENGTH_LONG).show();
+                break;
+
+            case 403:
+                Toast.makeText(this, "Not logged in", Toast.LENGTH_SHORT).show();
+                break;
+
+            default:
+                Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onError(Error err) {
+        progDialog.cancel();
+        Toast.makeText(this, err.getMessage(), Toast.LENGTH_SHORT).show();
     }
 }
--- AppVeranstalter/app/src/main/java/com/example/cora/sportverwaltungveranstalter/activity/events/MyEventsActivity.java
+++ AppVeranstalter/app/src/main/java/com/example/cora/sportverwaltungveranstalter/activity/events/MyEventsActivity.java
@@ -1,5 +1,6 @@
 package com.example.cora.sportverwaltungveranstalter.activity.events;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -70,5 +71,39 @@ public class MyEventsActivity extends BaseActivity implements AsyncTaskHandler{
         test.add(vertest);
         setAdapterData(test);
     }
+
+    @Override
+    public void onPreExecute() {
+        progDialog = new ProgressDialog(this);
+        progDialog.setMessage("Loading Events...");
+        progDialog.setIndeterminate(false);
+        progDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progDialog.setCancelable(false);
+        progDialog.show();
+    }
+
+    @Override
+    public void onSuccess(int statusCode, String content) {
+        progDialog.dismiss();
+        switch (statusCode) {
+            case 201:
+                Toast.makeText(MyEventsActivity.this, "Show all Events", Toast.LENGTH_LONG).show();
+                break;
+
+            case 403:
+                Toast.makeText(this, "Not logged in", Toast.LENGTH_SHORT).show();
+                break;
+
+            default:
+                Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onError(Error err) {
+        progDialog.cancel();
+        Toast.makeText(this, err.getMessage(), Toast.LENGTH_SHORT).show();
+    }
+
 
 }
